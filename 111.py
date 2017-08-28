03d56f060f09d9562caa1a7c5cc0c0c86e3513b4
358288433@qq.com <sun.chengwen@techvan.cn>
2017-08-28 14:10:19 +0800

    放进来了一个源程序

---
+++ 111.py
@@ -0,0 +1,17 @@
+print('-------------我是万恶的分割线--------------')
+import random
+secret = random.randint(1,10)
+while True :
+    temp = input ("我是孙成文 猜猜我心里想的数字:")
+    guess = int(temp)    
+    if guess == secret:
+        print ("我草，够牛逼的啊")
+        print("嗯，你猜对了，我给你个我的吻")
+    elif guess > secret:
+        print("傻屌你猜大了")
+        print ("拜拜了")
+    else:
+        print("傻屌你猜小了")
+        print ("拜拜了")
+    
+#
